d3e807a5731c79837df69d6e4ad3bb20548242ba
MrImmortal09 <omswami2004@gmail.com>
2024-04-16 05:28:43 +0530

    login fuctional

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,8 +1,41 @@
-import React from "react";
 import { AiOutlineTwitter } from "react-icons/ai";
 import { BiLogoFacebook } from "react-icons/bi";
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleLogin = async () => {
+    const loginData = { username, password };
+
+    try {
+      const response = await fetch('http://127.0.0.1:8080/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(loginData),
+      });
+
+      if (!response.ok) {
+        const errorMessage = await response.text();
+        throw new Error(errorMessage);
+      }
+      alert('Login successful!');
+      //redirect to home page
+      window.location.href = '/';
+      // Redirect or do something upon successful login
+      setMessage('Login successful!');
+    } catch (error) {
+      setMessage(error.message);
+    }
+
+  };
+  
+
+
   return (
     <section className="h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0">
       <div className="md:w-1/3 max-w-sm">
@@ -41,11 +74,15 @@ const Login = () => {
         <input
           className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded"
           type="text"
-          placeholder="Email Address"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
         />
         <input
           className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
           type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
           placeholder="Password"
         />
         <div className="mt-4 flex justify-between font-semibold text-sm">
@@ -64,9 +101,11 @@ const Login = () => {
           <button
             className="mt-4 bg-blue-600 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded text-xs tracking-wider"
             type="submit"
+            onClick={handleLogin}
           >
             Login
           </button>
+          {message && <p>{message}</p>}
         </div>
         <div className="mt-4 font-semibold text-sm text-slate-500 text-center md:text-left">
           Don&apos;t have an account?{" "}
