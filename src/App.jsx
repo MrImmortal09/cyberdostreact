93f1387f0c4ce7edee7405a2a766b1fb79ecaeef
PranavReddy0148 <pranav.katreddy0148@gmail.com>
2024-04-16 06:30:27 +0530

    Created GovtMeasures Page, and added it to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import './App.css';
-import Home from './components/pages/Home';
-import News from './components/pages/News';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import "./App.css";
+import Home from "./components/pages/Home";
+import News from "./components/pages/News";
+import GovtMeasures from "./components/pages/GMeasures";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <div className='w-full'>
+      <div className="w-full">
         <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/news' element={<News />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/news" element={<News />} />
+          <Route exact path="/GovtMeasures" element={<GovtMeasures />} />
           {/* Add more routes here */}
         </Routes>
       </div>
